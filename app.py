3741b50dd459b103d53d11eeccfd494ab9851f32
Robin Ghyselinck <rob@Robins-MacBook-Pro.local>
2020-08-09 09:59:38 +0200

    Class 3

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route('/<name>')
-def home(name):
-    return render_template('index.html', content=name, my_list=['Rob', 'Caro', 'Poupounch'])
+@app.route('/test/')
+def test():
+    return render_template('lamba.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
