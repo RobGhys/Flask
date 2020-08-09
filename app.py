78b7c63fe10ad6cc62c963b6825a58e95ed14537
Robin Ghyselinck <rob@Robins-MacBook-Pro.local>
2020-08-09 18:22:19 +0200

    Class 5

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request,session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = 'hello'
+app.permanent_session_lifetime = timedelta(days=30) # Stores session data for 30 days
 
 @app.route('/')
 def home():
@@ -16,17 +19,32 @@ def test():
 def login():
     # Redirects to user page
     if request.method == 'POST':
+        session.permanent = True # Needed in order to store session data
         user_name = request.form['nm']
-        return redirect(url_for('user', usr=user_name))
+        session['user'] = user_name
+        return redirect(url_for('user'))
+
     # Displays login page
     else:
+        # Redirects to user page if the user is already logged-in
+        if 'user' in session:
+            return redirect(url_for('user'))
         return render_template('login.html')
 
+@app.route('/logout/')
+def logout():
+    # Removes user data from session
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 
-@app.route('/<usr>/')
-def user(usr):
-    return f'<h1>{usr}</h1>'
+@app.route('/user/')
+def user():
+    if 'user' in session:
+        user_name = session['user']
+        return f'<h1>{user_name}</h1>'
+    else:
+        return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
