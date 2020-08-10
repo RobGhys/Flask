bad79332032726f207d7682eb8136e7550f665cd
Robin Ghyselinck <rob@Robins-MacBook-Pro.local>
2020-08-10 08:48:03 +0200

    Class 6

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-from flask import Flask, redirect, url_for, render_template, request,session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = 'hello'
-app.permanent_session_lifetime = timedelta(days=30) # Stores session data for 30 days
+app.permanent_session_lifetime = timedelta(days=5) # Stores session data for 30 days
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-@app.route('/test/')
-def test():
-    return render_template('lamba.html')
 
 
 @app.route('/login/', methods=['POST', 'GET'])
@@ -20,31 +15,36 @@ def login():
     # Redirects to user page
     if request.method == 'POST':
         session.permanent = True # Needed in order to store session data
-        user_name = request.form['nm']
-        session['user'] = user_name
+        user = request.form['nm']
+        session['user'] = user
+        flash(f"Logged in successful, {user}", "info")
         return redirect(url_for('user'))
 
     # Displays login page
     else:
         # Redirects to user page if the user is already logged-in
         if 'user' in session:
+            flash("Already logged in", "info")
             return redirect(url_for('user'))
         return render_template('login.html')
-
-@app.route('/logout/')
-def logout():
-    # Removes user data from session
-    session.pop('user', None)
-    return redirect(url_for('login'))
 
 
 @app.route('/user/')
 def user():
     if 'user' in session:
-        user_name = session['user']
-        return f'<h1>{user_name}</h1>'
+        user = session['user']
+        return render_template("user.html", user=user)
     else:
+        flash("You are not logged in", "info")
         return redirect(url_for('login'))
+
+
+@app.route('/logout/')
+def logout():
+    flash(f'You have been logged out!', 'info')
+    # Removes user data from session
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
